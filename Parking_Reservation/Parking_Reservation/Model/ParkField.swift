7fac3a474ec9fe0c4b4dde8b7a933675b3b60b94
zixinqian <zixin.qian-1@student.uts.edu.au>
2022-05-16 00:30:45 +0800

    add methods

--- Parking_Reservation/Parking_Reservation/Model/ParkField.swift
+++ Parking_Reservation/Parking_Reservation/Model/ParkField.swift
@@ -8,4 +8,23 @@ class ParkField {
         
     }
     
+    //return the number of empty slots that after users select time
+    func remainCount(date:Date, intervals:[Int]) -> Int {
+        var result:Int = 0;
+        for slot in mParkSlots {
+            if (!slot.isUsed(times: intervals)) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+    
+    //set slots value after users confirm
+    func bookSlot(date:Date, intervals:[Int]) {
+        for slot in mParkSlots {
+            if (!slot.isUsed(times: intervals)) {
+                slot.setUsed(times: intervals);
+            }
+        }
+    }
 }
